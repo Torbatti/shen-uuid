c4c807748a9a781211bd23f576b2d4f9dbad718a
torbatti <torbattii@gmail.com>
2025-02-28 19:17:11 +0330

    init:v1 uuid

--- src/rfc9562.zig
+++ src/rfc9562.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const root = @import("root");
+
 pub const Version = enum(u4) {
     unused = 0,
     time_based_greg = 1,
@@ -9,4 +12,29 @@ pub const Version = enum(u4) {
     time_based_epoch = 7,
     custom = 8,
     future,
+};
+
+pub const v1 = struct {
+    bytes: [16]u8 = [_]u8{0} ** 16,
+
+    pub fn init() void {
+        var uuid = v1{};
+        std.crypto.random.bytes(&uuid.bytes);
+
+        // var bytes: [16]u8 = [_]u8{0} ** 16;
+        // const bytes: [16]u8 = [_]u8{0} ** 16;
+
+        // 60-bit starting timestamp
+        const time = @as(u60, @intCast(@as(i60, @truncate(std.time.timestamp() >> 4))));
+
+        // const time = std.time.milliTimestamp();
+        // const time = @as(u60, @intCast(@truncate(std.time.timestamp() >> 4)));
+        // bytes[1] = @as(u8, @truncate(@as(u60, time << 52 >> 52)));
+        // bytes[2] = @as(u8, @truncate(@as(u60, time << 44 >> 52)));
+        // bytes[3] = @as(u8, @truncate(@as(u60, time << 36 >> 52)));
+
+        std.debug.print("{0b} ", .{time});
+        // std.debug.print("{any} ", .{bytes});
+
+    }
 };

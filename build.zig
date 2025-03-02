15ead5d6c1cb269ac05a7912186155832a147df3
torbatti <torbattii@gmail.com>
2025-03-02 15:45:15 +0330

    add: build options and build steps

--- build.zig
+++ build.zig
@@ -22,7 +22,17 @@ comptime {
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
+
+    // Top-level steps you can invoke on the command line.
+    // const build_steps = .{
+    //     .check = b.step("check", "Check if lib compiles"),
+    //     .static = b.step("static", "Build lib staticly"),
+    //     .@"test" = b.step("test", "Run all tests"),
+    // };
+
+    // Build options passed with `-D` flags.
+    // const build_options = .{};
 
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),

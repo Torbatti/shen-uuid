f09d6dda03425c840456ba4c8106313424fa04a6
torbatti <torbattii@gmail.com>
2025-03-02 17:05:05 +0330

    update: pub interface reworked

--- src/rfc9562.zig
+++ src/rfc9562.zig
@@ -15,18 +15,34 @@ pub const Version = enum(u4) {
     future,
 };
 
-pub const nil: u128 = 0x00000000000000000000000000000000;
-pub const max: u128 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+const nil: u128 = 0x00000000000000000000000000000000;
+const max: u128 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 test "nil and max" {
     _ = nil;
     _ = max;
     assert(nil == ~max);
+
+    _ = UUID.nil;
+    _ = UUID.max;
 }
 
-pub const v1 = struct {
-    bytes: [16]u8 = [_]u8{0} ** 16,
+pub const UUID = struct {
+    bytes: [16]u8,
 
-    pub fn init() v1 {
+    pub const nil: UUID = UUID{ .bytes = [_]u8{0x00} ** 16 };
+    pub const max: UUID = UUID{ .bytes = [_]u8{0xFF} ** 16 };
+
+    pub fn reset(uuid: *UUID) void {
+        uuid.*.bytes = [_]u8{0x00} ** 16;
+    }
+
+    pub const new_v4 = UUID_V4.new;
+};
+
+const v1 = struct {
+    bytes: [16]u8,
+
+    fn init() v1 {
         var uuid = v1{};
 
         // 60-bit starting timestamp
@@ -65,18 +81,16 @@ pub const v1 = struct {
     }
 };
 
-pub const v4 = struct {
-    bytes: [16]u8 = [_]u8{0} ** 16,
-
-    pub fn init() v4 {
-        var uuid = v4{};
-        std.crypto.random.bytes(&uuid.bytes);
+// time_low 32 | time_mid 16 | version 4 | time_high 12 | variant 2 | clock_seq 14 |  node 48
+const UUID_V4 = struct {
+    fn new(uuid: *UUID) void {
+        std.crypto.random.bytes(uuid.bytes);
 
         // Version 4
-        uuid.bytes[6] = (uuid.bytes[6] & 0x0f) | 0x40;
+        uuid.*.bytes[6] = (uuid.bytes[6] & 0x0f) | 0x40;
 
         // Variant RFC9562
-        uuid.bytes[8] = (uuid.bytes[8] & 0x3f) | 0x80;
+        uuid.*.bytes[8] = (uuid.bytes[8] & 0x3f) | 0x80;
 
         return uuid;
     }

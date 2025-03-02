fc4782564b3926e7fa1c5f1e486459a22a2109b4
torbatti <torbattii@gmail.com>
2025-03-02 17:03:24 +0330

    interface rework

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,3 @@
 const utils = @import("utils.zig");
 
-const rfc9562 = @import("rfc9562.zig");
-pub const uuid_nil = rfc9562.nil;
-pub const uuid_max = rfc9562.max;
-// pub const uuid_v1 = rfc9562.v1;
-pub const uuid_v4 = rfc9562.v4;
+pub const rfc9562 = @import("rfc9562.zig");
